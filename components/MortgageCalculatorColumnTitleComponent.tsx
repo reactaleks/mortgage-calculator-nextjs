3b82f2dfbdcf274ac2769dec6411d353dada7a9b
Aleks <a.ivanov.developer@gmail.com>
2024-07-05 12:08:29 +0100

    Adjusted col title heading, and stop from wrapping Adjusted padding for columns

--- components/MortgageCalculatorColumnTitleComponent.tsx
+++ components/MortgageCalculatorColumnTitleComponent.tsx
@@ -1,7 +1,7 @@
 export default function CalculatorColumnTitle({columnTitle, textalaignment}) {
     return (
         <div className="p-5 mortgage-calculator-column-title">
-            <h2 className={`mortgage-calculator-column-title-heading text-xl font-bold ${textalaignment}`}>{columnTitle}</h2>
+            <h2 className={`mortgage-calculator-column-title-heading text-nowrap text-2xl text-darker-blue font-extrabold ${textalaignment}`}>{columnTitle}</h2>
         </div>
     )
 }
